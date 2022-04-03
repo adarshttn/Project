87212af67fb9d2d1063de7b833ce43f49e8f8a7c
Adarsh Gautam <adarsh.gautam@tothenew.com>
2022-04-03 11:34:16 +0530

    new change

---
+++ my-app/src/components/Login/log.jsx
@@ -0,0 +1,25 @@
+
+ function log() {
+  return (
+    
+
+    <main className='flex flex-col items-center w-full flex-1 px-20 text-center'>
+
+          <div className='bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl'>
+            <div className="w-2/5 p-5">
+              <p>Sign iup Section</p>
+
+            </div>
+            <div className="w-3/5 p-5 bg-white rounded-2xl rounded-br-2xl">
+              <p>Sign in Section</p>
+
+
+            </div>
+          
+          </div>
+
+    </main>
+
+  )
+}
+export default log

52813e73cac6a9ed605461875376d0ef82bad224
Adarsh Gautam <adarsh.gautam@tothenew.com>
2022-04-05 11:21:59 +0530

    new change

--- my-app/src/components/Topbar/Topbar.js
+++ my-app/src/components/Topbar/Topbar.js
@@ -3,6 +3,10 @@ import './Topbar.css'
 
 export default function Topbar() {
   return (
-    <div className='topbar col-span-2 h-9 bg-green-400 h-24'>Topbar</div>
+    <div className='topbar col-span-2  bg-red-100 p-2 h-9 rounded-full'>
+
+      <input className='flex ml-1 items-center rounded-full bg-gray-100 ' type="text" />
+      
+      Topbar</div>
   )
 }

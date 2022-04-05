bd213402929c2a2b2e465304b928ea1925846a16
Adarsh Gautam <adarsh.gautam@tothenew.com>
2022-04-05 14:38:56 +0530

    new change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
- import {ToastContainer} from 'react-toastify'
+ 
  import 'react-toastify/dist/ReactToastify.css'
 import react from "react";
 

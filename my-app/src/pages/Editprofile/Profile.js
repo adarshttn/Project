41dc0c8b4ab8f035f05a7fa49e8bc55ae71b2012
abhishek <abhishek.gupta2@tothenew.com>
2022-04-13 18:25:13 +0530

    navbar

--- my-app/src/pages/Editprofile/Profile.js
+++ my-app/src/pages/Editprofile/Profile.js
@@ -98,20 +98,28 @@ function Profile() {
                     />
                   </div>
 
-                  <div class="w-full md:w-1/2 px-3 mt-3">
+                  <div class="w-full md:w-1/2 px-3 mt-3  ">
                     <label
                       class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
                       for="grid-last-name"
                     >
                       Gender
                     </label>
-                    <input
+                    <div className="flex gap-5 appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                    <button className="bg-blue-300 p-2 rounded text-xm pl-4 pr-4 font-medium">
+                        Male
+                    </button>
+                    <button className="bg-pink-300 p-2 rounded text-xm font-medium">
+                        Female
+                    </button>
+                    </div>
+                    {/* <input
                       class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       id=""
                       type="text"
 
-                      placeholder=""
-                    />
+                      placeholder="" */}
+                   
                   </div>
 
                   <div class="w-full md:w-1/2 px-3 mt-3">

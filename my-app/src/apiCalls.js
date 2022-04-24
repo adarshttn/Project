2a6b3e1cf41d2938da8b01a455368dcfc092e956
Adarsh Gautam <adarsh.gautam@tothenew.com>
2022-04-24 13:02:23 +0530

    edit profile

--- my-app/src/apiCalls.js
+++ my-app/src/apiCalls.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+// import{
+//   USER_UPDATE_REQUEST,
+// } from "./context/AuthActions";
 
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
@@ -9,3 +12,19 @@ export const loginCall = async (userCredential, dispatch) => {
     dispatch({ type: "LOGIN_FAILURE", payload: err });
   }
 };
+
+// update user
+
+// export const userprofileReducer = async (state ={}, action) => {
+//   switch (action.type) {
+//     case "USER_UPDATE_REQUEST":
+//       return { loading: true};
+//     case "USER_UPDATE_SUCCESS":
+//       return {loading:false,success:true, user:action.payload};
+//     case "USER_UPDATE_FAIL":
+//       return {loading:false,success:false, error:action.payload};
+//       default:
+//         return state;
+//   }
+
+// }
--- my-app/src/context/AuthActions.js
+++ my-app/src/context/AuthActions.js
@@ -14,3 +14,9 @@ export const LoginStart = (userCredentials) => ({
   export const logOut=()=>({
     type:"LOGOUT",
   })
+
+//UPDATE_USER
+  export const USER_UPDATE_REQUEST ='USER_UPDATE_REQUEST'
+  export const USER_UPDATE_SUCCESS ='USER_UPDATE_SUCCESS'
+  export const USER_UPDATE_FAIL ='USER_UPDATE_ FAIL'
+  export const USER_UPDATE_RESET ='USER_UPDATE_ RESET'
